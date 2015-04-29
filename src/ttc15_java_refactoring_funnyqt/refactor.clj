d06142c9edd9d4f58036a3312932a441d8642f4e
Tassilo Horn <tsdh@gnu.org>
2015-04-29 21:49:50 +0200

    use generated api in refactor

--- src/ttc15_java_refactoring_funnyqt/refactor.clj
+++ src/ttc15_java_refactoring_funnyqt/refactor.clj
@@ -5,42 +5,40 @@
             [funnyqt.query    :refer [member? forall? the]]
             [funnyqt.generic  :refer [has-type?]]
             [funnyqt.in-place :refer :all]
-            [funnyqt.utils    :refer [errorf]])
+            [funnyqt.utils    :refer [errorf]]
+            [ttc15-java-refactoring-funnyqt.mm.java :as j]
+            [ttc15-java-refactoring-funnyqt.mm.pg   :as pg])
   (:import (ttc.testdsl.tTCTest Java_Class Java_Method)
            (org.eclipse.emf.ecore.resource Resource ResourceSet)))
 
 ;;* Utils
 
 (defn find-tclass [pg qn]
-  (first (filter #(= qn (eget-raw % :tName))
-                 (eallcontents pg 'TClass))))
+  (first (filter #(= qn (pg/tName %))
+                 (pg/eall-TClasses pg))))
 
 (defn find-tmethodsig [pg method-name param-qns]
   (let [pclasses (mapv (partial find-tclass pg) param-qns)]
-    (first (filter (fn [tms]
-                     (and (-> tms
-                              (eget-raw :method)
-                              (eget-raw :tName)
-                              (= method-name))
-                          (= pclasses (eget-raw tms :paramList))))
-                   (eallcontents pg 'TMethodSignature)))))
+    (first (filter #(and (-> % pg/->method pg/tName (= method-name))
+                         (= pclasses (pg/->paramList %)))
+                   (pg/eall-TMethodSignatures pg)))))
 
 ;;* Task 2: Refactoring on the Program Graph
 
 (defn ^:private superclass? [super sub]
-  (loop [sub-super (eget-raw sub :parentClass)]
+  (loop [sub-super (pg/->parentClass sub)]
     (when sub-super
       (or (= sub-super super)
-          (recur (eget-raw sub-super :parentClass))))))
+          (recur (pg/->parentClass sub-super))))))
 
 (defn ^:private accessible-from? [cls m-or-f]
   (let [defining-cls (econtainer m-or-f)]
-    (or (= defining-cls cls)                 ;; own members
-        (superclass? defining-cls cls)       ;; inherited members
-        (not (superclass? cls defining-cls)) ;; unrelated classes
-        (and (has-type? m-or-f 'TMethodDefinition) ;; subclass-method but with override
-             (member? (eget-raw m-or-f :signature)
-                      (eget-raw cls :signature))
+    (or (= defining-cls cls)                    ;; own members
+        (superclass? defining-cls cls)          ;; inherited members
+        (not (superclass? cls defining-cls))    ;; unrelated classes
+        (and (pg/isa-TMethodDefinition? m-or-f) ;; subclass-method but with override
+             (member? (pg/->signature m-or-f)
+                      (pg/->signature cls))
              (superclass? cls defining-cls)))))
 
 (defrule pull-up-method [pg pg2jamopp-map-atom super sig]
@@ -53,43 +51,43 @@
    ;; super doesn't have such a method of this signature already
    super -!<:signature>-> sig
    ;; Really all subclasses define a method with sig
-   :when (= (count (eget super :childClasses))
+   :when (= (count (pg/->childClasses super))
             (inc (count others)))
    :let [all-mds (conj (map :omd others) md)]
    ;; All accesses from all method-defs must be accessible already from the
    ;; superclass.
    :when (forall? (partial accessible-from? super)
                   (mapcat #(eget % :access) all-mds))]
-  (println "Pulling up" (-> sig (eget :method) (eget :tName)) "to" (eget super :tName))
+  (println "Pulling up" (-> sig pg/->method pg/tName) "to" (pg/tName super))
   ;; Delete the other method defs
   (doseq [o others]
     (edelete! (:omd o)))
   ;; Add it to the super class
-  (eadd! super :defines md)
-  (eadd! super :signature sig)
+  (pg/->add-defines! super md)
+  (pg/->add-signature! super sig)
   (fn [_]
     (doseq [o others]
       (edelete! (@pg2jamopp-map-atom (:omd o)))
       (swap! pg2jamopp-map-atom dissoc (:omd o)))
-    (eadd! (@pg2jamopp-map-atom super) :members (@pg2jamopp-map-atom md))))
+    (j/->add-members! (@pg2jamopp-map-atom super) (@pg2jamopp-map-atom md))))
 
 (defn make-type-reference [target-class]
-  (ecreate! nil 'NamespaceClassifierReference
-            {:namespaces (eget (econtainer target-class) :namespaces)
-             :classifierReferences [(ecreate! nil 'ClassifierReference
-                                              {:target target-class})]}))
+  (j/create-NamespaceClassifierReference!
+   nil {:namespaces (j/namespaces (econtainer target-class))
+        :classifierReferences [(j/create-ClassifierReference!
+                                nil {:target target-class})]}))
 
 (defn create-superclass [pg pg2jamopp-map-atom classes new-superclass-qn]
-  (let [scs (into #{} (map #(eget-raw % :parentClass)) classes)]
+  (let [scs (into #{} (map pg/->parentClass) classes)]
     (when (and (= 1 (count scs))
                (not (find-tclass pg new-superclass-qn)))
       (println "Creating new superclass" new-superclass-qn
                (if (first scs)
-                 (str "with parent" (eget (first scs) :tName))
+                 (str "with parent" (pg/tName (first scs)))
                  "with no parent"))
-      (let [new-tclass (ecreate! pg 'TClass {:tName new-superclass-qn
-                                             :childClasses classes
-                                             :parentClass (first scs)})]
+      (let [new-tclass (pg/create-TClass! pg {:tName new-superclass-qn
+                                              :childClasses classes
+                                              :parentClass (first scs)})]
         (fn [^ResourceSet rs]
           (let [[pkgs class-name] (let [parts (str/split new-superclass-qn #"\.")]
                                     [(butlast parts) (last parts)])
@@ -98,13 +96,13 @@
                                              (re-matches #"(.*/src/).*")
                                              second)
                                         (str/join "/" pkgs) "/" class-name ".java"))
-                nc (ecreate! nil 'Class {:name class-name
-                                         :annotationsAndModifiers [(ecreate! nil 'Public)]})
-                cu (ecreate! r 'CompilationUnit {:name (str new-superclass-qn ".java")
+                nc (j/create-Class! nil {:name class-name
+                                         :annotationsAndModifiers [(j/create-Public! nil)]})
+                cu (j/create-CompilationUnit! r {:name (str new-superclass-qn ".java")
                                                  :namespaces  pkgs
                                                  :classifiers [nc]})]
             (doseq [c classes]
-              (eset! (@pg2jamopp-map-atom c) :extends (make-type-reference nc)))
+              (j/->set-extends! (@pg2jamopp-map-atom c) (make-type-reference nc)))
             (when-let [parent (first scs)]
-              (eset! nc :extends (make-type-reference (@pg2jamopp-map-atom parent))))
+              (j/->set-extends! nc (make-type-reference (@pg2jamopp-map-atom parent))))
             (swap! pg2jamopp-map-atom assoc new-tclass nc)))))))
